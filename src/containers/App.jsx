3769c6b81683527e1bb89a10a1b3fcb54249f0db
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-02-09 11:52:51 +0500

    style

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -8,13 +8,6 @@ function App() {
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchfield] = useState('');
 
-    // useEffect(async() => {
-    //     const response = await fetch ('https://jsonplaceholder.typicode.com/users');
-    //     const data = await response.json;
-    //     const [user] = data;
-    //     setRobots(user)
-    // }, [])
-
     useEffect(()=>{
         fetch ('https://jsonplaceholder.typicode.com/users')
         .then(response=>response.json())
